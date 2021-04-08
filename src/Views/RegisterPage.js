691183692e388b0c29ad5d3dbb20c4dc067e421a
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-08 22:17:58 +0300

    Added basic registration form.

--- src/Views/RegisterPage.js
+++ src/Views/RegisterPage.js
@@ -1,17 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Section from '../components/Section';
 import Main from '../components/Main';
 
-const RegisterPage = () => {
-  return (
-    <Main>
+class RegisterPage extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // this.props.onRegister(this.state);
+
+    this.setState({ name: '', email: '', password: '' });
+  };
+
+  render() {
+    const { name, email, password } = this.state;
+
+    return (
       <Section>
-        <div>
-          <h2>Register Page</h2>
-        </div>
+        <Main>
+          <h1>Registration Page</h1>
+
+          <form onSubmit={this.handleSubmit} autoComplete="off">
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <label>
+              Email
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <label>
+              Password
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <button type="submit">Register</button>
+          </form>
+        </Main>
       </Section>
-    </Main>
-  );
-};
+    );
+  }
+}
 
 export default RegisterPage;
