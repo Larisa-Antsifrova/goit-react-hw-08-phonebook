4f3c2f936235b3a4ce45e7b04aa13cf9a2a33e32
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-10 00:59:29 +0300

    Removed page title. Refactored Contacts page.

--- src/Views/ContactsPage.js
+++ src/Views/ContactsPage.js
@@ -1,28 +1,26 @@
+// Imports from React
 import React from 'react';
 // Imports of components
+import Main from '../components/Main';
 import Section from '../components/Section';
-import PageTitle from '../components/PageTitle';
 import Title from '../components/Title';
 import ContactForm from '../components/ContactForm';
 import Filter from '../components/Filter';
 import ContactList from '../components/ContactList';
-import Main from '../components/Main';
 
-const App = () => {
+const ContactsPage = () => {
   return (
-    <>
-      <Main>
-        <PageTitle title="Phone Book" />
-        <Section>
-          <Title title="Add contacts" />
-          <ContactForm />
-          <Title title="Сontacts" />
-          <Filter />
-          <ContactList />
-        </Section>
-      </Main>
-    </>
+    <Main>
+      <Section>
+        <Title title="Add contacts" />
+        <ContactForm />
+        <Title title="Find contacts" />
+        <Filter />
+        <Title title="Contacts" />
+        <ContactList />
+      </Section>
+    </Main>
   );
 };
 
-export default App;
+export default ContactsPage;
