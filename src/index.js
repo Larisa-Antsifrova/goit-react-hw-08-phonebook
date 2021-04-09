1358f101c974ec367bfb1cd5b3b72ca688a36ebb
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-09 15:26:27 +0300

    Added Persist to keep auth token.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 // Imports from Redux
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
-// import { store, persistor } from './redux/store';
-//Imports from Redux Persist
-// import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+// Imports from Redux Persist
+import { PersistGate } from 'redux-persist/integration/react';
+
 // Imports of components
 import App from './App';
+
 // Styles imports
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
@@ -18,9 +20,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        {/* <PersistGate loading="Loading..." persistor={persistor}> */}
-        <App />
-        {/* </PersistGate> */}
+        <PersistGate loading="Loading..." persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,48 @@
 // Imports from Redux Toolkit
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// Imports from Redux Persist
+import storage from 'redux-persist/lib/storage';
+// Redux Persist fix
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 // Imports of middleware
 import logger from 'redux-logger';
 // Imports of reducers
 import { contactsReducer } from './contacts/contacts-reducers';
 import { authReducer } from './auth/auth-reducers';
 
-const middleware = [...getDefaultMiddleware(), logger];
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const authPersistConfig = {
+  key: 'token',
+  storage,
+  whitelist: ['token'],
+};
 
 const store = configureStore({
   reducer: {
     contacts: contactsReducer,
-    auth: authReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
   },
   middleware,
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor };
