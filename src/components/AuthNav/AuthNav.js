f55aa28da6a72ddcc68af295c794713be2bd35b2
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-11 01:44:46 +0300

    Refactored AuthNav component.

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,14 +1,33 @@
+// Imports from React
 import React from 'react';
-
 import { NavLink } from 'react-router-dom';
+// Imports of routes
 import routes from '../../routes';
+// Styles imports
+import styles from './AuthNav.module.css';
 
 const AuthNav = () => {
   return (
-    <div>
-      <NavLink to={routes.register}>Register</NavLink>
-      <NavLink to={routes.login}>Login</NavLink>
-    </div>
+    <ul className={styles.NavLinksList}>
+      <li>
+        <NavLink
+          to={routes.register}
+          className={styles.NavLink}
+          activeClassName={styles['NavLink-active']}
+        >
+          Register
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={routes.login}
+          className={styles.NavLink}
+          activeClassName={styles['NavLink-active']}
+        >
+          Login
+        </NavLink>
+      </li>
+    </ul>
   );
 };
 
